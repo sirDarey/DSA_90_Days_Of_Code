e6c1068bc542cc36a8fd34585ef1a7c5ee640902
Sir Darey <darey1oguns@gmail.com>
2023-04-28 09:28:52 +0100

    Day 33

---
+++ src/Day_33_196_DeleteDuplicateEmails.java
@@ -0,0 +1,12 @@
+
+/***
+ * 
+ * @author Sir Darey
+ * 
+ * Day 33: LeetCode Problem 196 - Delete Duplicate Emails
+ */
+
+public class Day_33_196_DeleteDuplicateEmails {
+	
+	//SQL: DELETE p2 from person p1, person p2 where p1.email=p2.email and p2.id>p1.id;
+}
