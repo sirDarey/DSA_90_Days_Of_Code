66745ed1171ebb906b870a9339aaa282990d0053
Sir Darey <darey1oguns@gmail.com>
2023-05-30 20:57:31 +0100

    Day 58

---
+++ src/Day_58_18_4Sum.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/***
+ * 
+ * @author Sir Darey
+ * 
+ * Day 58: LeetCode Problem 18 - 4Sum
+ * 
+ * 
+ */
+
+public class Day_58_18_4Sum {
+	
+	public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
+        int n = nums.length;
+        // Sorting the array
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        // Skip duplicates
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return quadruplets;
+    }  
+}
