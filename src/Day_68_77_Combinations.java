48051df4e70d0cd047ca3b0f35c0279f88d0cfdb
Sir Darey <darey1oguns@gmail.com>
2023-07-21 10:49:48 +0100

    Day 68

---
+++ src/Day_68_77_Combinations.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * 
+ * @author Sir Darey
+ * 
+ * Day 68: LeetCode Problem 77 - Combinations
+ * 
+ * 
+ */
+
+public class Day_68_77_Combinations {
+	
+	List<List<Integer>> result = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    int n, k;
+
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n; this.k = k;
+        backtrack(1);
+
+        return result;
+    }
+
+    void backtrack(int i) {
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int j=i; j<=n; j++) {
+            if (temp.contains(j))
+                continue;
+
+            temp.add(j);
+            backtrack(j+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
