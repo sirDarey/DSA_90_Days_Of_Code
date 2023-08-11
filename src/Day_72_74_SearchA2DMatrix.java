9c9cef076d84ff27b701fe3e2cdcc8ffb1d53447
Sir Darey <darey1oguns@gmail.com>
2023-08-11 10:27:29 +0100

    Day 72

---
+++ src/Day_72_74_SearchA2DMatrix.java
@@ -0,0 +1,42 @@
+
+
+/**  
+* 
+*  @Sir Darey
+* 
+* Day 72: Problem 74 - Search a 2D Matrix
+* 
+*/
+
+public class Day_72_74_SearchA2DMatrix {
+	
+	public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        
+        int up = 0, down = m-1, c = n-1, mid = 0;
+        while (up <= down) {
+            mid = (up+down)/2;
+            if (matrix[mid][0] > target)
+                down = mid - 1;
+            else if (matrix[mid][c] < target)
+                up = mid + 1;
+            else if (matrix[mid][c] > target)
+                break;
+            else
+                return true;
+        }
+
+        int left = 0, right = c, r = mid;
+        while (left <= right) {
+            mid = (left+right)/2;
+            int current = matrix[r][mid];
+            if (current > target)
+                right = mid - 1;
+            else if (current < target)
+                left = mid + 1;
+            else
+                return true;
+        }
+        return false;
+    }
+}
